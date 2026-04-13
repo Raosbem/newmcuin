b6d514ac73265a004aba6f2893813a44d2a6c3e9
Raosbem <rangelbernardo188@gmail.com>
2026-04-13 16:52:45 -0600

    re ajuste
    aa

--- autopartes-api/entrypoint.sh
+++ autopartes-api/entrypoint.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e
 
+echo "Esperando conexión a la BD..."
+until python -c "
+import psycopg2, os, sys
+try:
+    psycopg2.connect(os.environ['DATABASE_URL'])
+    sys.exit(0)
+except Exception as e:
+    print(f'BD no disponible: {e}', flush=True)
+    sys.exit(1)
+" 2>&1; do
+  echo "Reintentando en 3s..."
+  sleep 3
+done
+
 echo "Corriendo migraciones de Alembic..."
 alembic upgrade head
 
